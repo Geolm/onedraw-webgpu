ade85da81220f0a2557439fb214a8c081391e867
Geolm <moyaguillaume@gmail.com>
2026-03-05 18:02:02 +0100

    added default font generation

--- prebuild/builder.c
+++ prebuild/builder.c
@@ -9,10 +9,32 @@
 #define ARENA_IMPLEMENTATION
 #include "arena.h"
 
+#include "../lib/onedraw.h"
+
 // ---------------------------------------------------------------------------------------------------------------------------
 #define UNUSED_VARIABLE(a) (void)(a)
 #define ONEDRAW_MAJOR_VERSION (0)
 #define ONEDRAW_MINOR_VERSION (1)
+#define FONT_CHAR_FIRST 33
+#define FONT_CHAR_LAST 126
+#define FONT_NUM_CHARS 95
+
+// ---------------------------------------------------------------------------------------------------------------------------
+void* read_file(const char* filename, size_t* file_size, Arena* arena)
+{
+    FILE* f = fopen(filename, "rb");
+    if (f != NULL)
+    {
+        fseek(f, 0L, SEEK_END);
+        *file_size = ftell(f);
+        fseek(f, 0L, SEEK_SET);
+
+        void* buffer = arena_alloc(arena, (*file_size));
+        if (fread(buffer, *file_size, 1, f) == 1)
+            return buffer;
+    }
+    return NULL;
+}
 
 //----------------------------------------------------------------------------------------------------------------------------
 char* read_shader(const char* filename, size_t* string_size, Arena* arena)
@@ -36,7 +58,7 @@ char* read_shader(const char* filename, size_t* string_size, Arena* arena)
 }
 
 //----------------------------------------------------------------------------------------------------------------------------
-int export_shaders(Arena* arena)
+bool export_shaders(Arena* arena)
 {
     fprintf(stdout, "=> exporting shaders ");
     
@@ -45,7 +67,7 @@ int export_shaders(Arena* arena)
     if (binning_shader_data == NULL)
     {
         fprintf(stdout, "\nshader not found\n\n");
-        return -1;
+        return false;
     }
     string2h("lib/binning.h", "binning_shader", binning_shader_data, binning_shader_length);
     fprintf(stdout, ".");
@@ -55,12 +77,83 @@ int export_shaders(Arena* arena)
     if (rasterizer_shader_data == NULL)
     {
         fprintf(stdout, "\nshader not found\n\n");
-        return -1;
+        return false;
     }
     string2h("lib/rasterizer.h", "rasterizer_shader", rasterizer_shader_data, rasterizer_shader_length);
     fprintf(stdout, ".\n\n");
 
-    return 0;
+    return true;
+}
+
+// ---------------------------------------------------------------------------------------------------------------------------
+bool build_font(float font_height, uint32_t atlas_width, uint32_t atlas_height, Arena *arena)
+{
+    fprintf(stdout, "=> generating font\n");
+
+    if ((atlas_width%4) != 0 || (atlas_height%4) != 0)
+    {
+        fprintf(stdout, "font atlas width and height must be a multiple of 4\n");
+        return false;
+    }
+
+    size_t font_size;
+    uint8_t* font_data = read_file("fonts/Satoshi-Medium.otf", &font_size, arena);
+
+    if (font_data == NULL)
+        return false;
+
+    fprintf(stdout, "baking %dx%d atlas : ", atlas_width, atlas_height);
+
+    uint8_t* atlas_pixels = arena_alloc(arena, atlas_height * atlas_width);
+    if (atlas_pixels == NULL)
+        return false;
+
+    stbtt_bakedchar glyphs[FONT_NUM_CHARS];
+    int result = stbtt_BakeFontBitmap(font_data, 0, font_height, atlas_pixels, atlas_width, atlas_height,
+                                      FONT_CHAR_FIRST, FONT_NUM_CHARS, glyphs);
+
+    if (result == 0)
+        return false;
+    else if (result < 0)
+        fprintf(stdout, "warning only %d chars could fit in the atlas\n", -result);
+    else fprintf(stdout, "ok\n");
+
+    // font atlas is uncompressed to target more platforms
+    if (!bin2h("lib/default_font_atlas.h", "default_font_atlas", font_data, atlas_width * atlas_height))
+        return false;
+
+    fprintf(stdout, "filling glyphs structure : ");
+
+    od_font* font = arena_alloc(arena, sizeof(od_font));
+    if (font == NULL)
+        return false;
+
+    memset(font, 0, sizeof(od_font));
+    font->first_glyph = FONT_CHAR_FIRST;
+    font->num_glyphs = FONT_NUM_CHARS;
+    font->texture_width = atlas_width;
+    font->texture_height = atlas_height;
+    font->font_height = font_height;
+
+    for(uint32_t i=0; i<FONT_NUM_CHARS; ++i)
+    {
+        font->glyphs[i] = (od_glyph)
+        {
+            .x0 = glyphs[i].x0,
+            .y0 = glyphs[i].y0,
+            .x1 = glyphs[i].x1,
+            .y1 = glyphs[i].y1,
+            .bearing_x = glyphs[i].xoff,
+            .bearing_y = glyphs[i].yoff,
+            .advance_x = glyphs[i].xadvance
+        };
+    }
+
+    if (!bin2h("lib/default_font.h", "default_font", font, sizeof(od_font)))
+        return false;
+
+    fprintf(stdout, "ok\n\n");
+    return true;
 }
 
 //----------------------------------------------------------------------------------------------------------------------------
@@ -73,7 +166,10 @@ int main(int argc, const char * argv[])
 
     fprintf(stdout, "onedraw-webgpu %u.%u library builder\n\n", ONEDRAW_MAJOR_VERSION, ONEDRAW_MINOR_VERSION);
 
-    if (export_shaders(&arena) != 0)
+    if (!export_shaders(&arena))
+        return -1;
+
+    if (!build_font(32.f, 256, 256, &arena))
         return -1;
 
     arena_free(&arena);
